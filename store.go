2ace8011f87eeeed3fa5194ade578dee9dd2eeee
Ahmed Mze <houdjiva@gmail.com>
2022-07-06 15:43:24 +0200

    Création de l'interface Store

---
+++ store.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
+
+type Store interface {
+	Open() error
+	Close() error
+}
+
+type dbStore struct {
+	db *sqlx.DB
+}
+
+func (store *dbStore) Open() error {
+	db, err := sqlx.Connect("sqlite3", "goflix.db")
+	if err != nil {
+		return err
+	}
+	log.Println("Connected to DB")
+	store.db = db
+	return nil
+}
+
+func (store *dbStore) Close() error {
+	return store.db.Close()
+}
