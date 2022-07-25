9db9d2d2a15aca41b14ec0b4c79f68a8ae3f862d
Ahmed Mze <houdjiva@gmail.com>
2022-07-25 15:28:23 +0200

    Ecriture du test d'integration

--- routes_movie_test.go
+++ routes_movie_test.go
@@ -71,3 +71,33 @@ func TestMovieCreateUnit(t *testing.T) {
 	srv.handleMovieCreate()(w, r)
 	assert.Equal(t, http.StatusOK, w.Code)
 }
+
+func TestMovieCreateIntegration(t *testing.T) {
+	// Création du server avec test de base de donnée
+	srv := newServer()
+	srv.store = &testSore{}
+
+	// Préparation du BODY JSON
+	p := struct {
+		Title       string `json:"title"`
+		ReleaseDate string `json:"release_date"`
+		Duration    int64  `json:"duration"`
+		TrailerUrl  string `json:"trailer_url"`
+	}{
+		Title:       "Incepion",
+		ReleaseDate: "2010-07-18",
+		Duration:    148,
+		TrailerUrl:  "http://url",
+	}
+	var buf bytes.Buffer
+	err := json.NewEncoder(&buf).Encode(p)
+
+	assert.Nil(t, err)
+
+	w := httptest.NewRecorder()
+	r := httptest.NewRequest("POST", "/api/movies/", &buf)
+
+	//srv.handleMovieCreate()(w, r)
+	srv.serveHTTP(w, r)
+	assert.Equal(t, http.StatusOK, w.Code)
+}
