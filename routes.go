dfec7bb09ff5abf3b764d53499cd36ffcb47dbef
Ahmed Mze <houdjiva@gmail.com>
2022-07-25 16:59:54 +0200

    Implementation du JWT

--- routes.go
+++ routes.go
@@ -3,6 +3,7 @@ package main
 // Mapping des différentes routes
 func (s *server) routes() {
 	s.router.HandleFunc("/", s.handleIndex()).Methods("GET")
+	s.router.HandleFunc("/api/token", s.handleTokenCreate()).Methods("POST")
 	s.router.HandleFunc("/api/movies/{id:[0-9]+}", s.handleMovieDetail()).Methods("GET")
 	s.router.HandleFunc("/api/movies/", s.handleMovieList()).Methods("GET")
 	s.router.HandleFunc("/api/movies/", s.handleMovieCreate()).Methods("POST")
--- routes_auth.go
+++ routes_auth.go
@@ -2,12 +2,73 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
 func (s *server) handleIndex() http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
 		//fmt.Fprintf(writer, "Welcome to Goflix")
-		_, _ = fmt.Fprintf(writer, "Welcome to Goflix")
+		//_, _ = fmt.Fprintf(writer, "Welcome to Goflix")
+	}
+}
+
+func (s *server) handleTokenCreate() http.HandlerFunc {
+	type request struct {
+		Username string `json:"username"`
+		Password string `json:"password"`
+	}
+
+	type response struct {
+		Token string `json:"token"`
+	}
+
+	type responseError struct {
+		Error string `json:"error"`
+	}
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		// Parsing du body
+		req := request{}
+		err := s.decode(w, r, &req)
+		if err != nil {
+			msg := fmt.Sprintf("Cannot parse login body. err=%v", err)
+			log.Println(msg)
+			s.respond(w, r, responseError{
+				Error: msg,
+			}, http.StatusBadRequest)
+			return
+		}
+
+		// Vérification des identifiants
+		if req.Username != "golang" || req.Password != "rocks" {
+			s.respond(w, r, responseError{
+				Error: "Invalid credentials",
+			}, http.StatusUnauthorized)
+			return
+		}
+
+		// Création du token
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+			"username": req.Username,
+			"exp":      time.Now().Add(time.Hour * time.Duration(1)).Unix(),
+			"iat":      time.Now().Unix(),
+		})
+
+		tokenStr, err := token.SignedString([]byte(JWT_APP_KEY))
+		if err != nil {
+			msg := fmt.Sprintf("Cannot generate JWT. err=%v", err)
+			s.respond(w, r, responseError{
+				Error: msg,
+			}, http.StatusInternalServerError)
+			return
+		}
+
+		s.respond(w, r, response{
+			Token: tokenStr,
+		}, http.StatusOK)
 	}
 }
